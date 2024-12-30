fe497517eeaf44d3afc66cfcba66271f01feea1b
Dinithi Piyathilaka <dinipiyathilaka@gmail.com>
2024-12-31 01:14:43 +0530

    Implement function to send data from frontend to backend

--- FrontEnd/js/employee.js
+++ FrontEnd/js/employee.js
@@ -4,5 +4,24 @@ function saveEmployee(){
     let address=$('#exampleFormControlInput3').val();
     let number=$('#exampleFormControlInput4').val();
 
-    console.log(name)
+    $.ajax({
+        method:"POST",
+        contentType:"application/json",
+        url:"http://localhost:8080/api/v1/employee/saveEmployee",
+        async:true,
+        data:JSON.stringify({
+            "empID":"",
+            "empName":name,
+            "empAddress":address,
+            "empMNumber":number
+        }),
+        success: function (data) {
+            alert("saved")
+            getAllEmployees()
+        },
+        error: function (xhr, exception) {
+            alert("rrrrError")
+        }
+    })
+
 }
